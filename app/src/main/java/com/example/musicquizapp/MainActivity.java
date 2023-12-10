6c4f88e69c96bfa84cd1b94cc92b8a24efa674b3
DaoLongVu20130466 <daolongvu2107202@gmail.com>
2023-12-10 12:12:21 +0700

    push set up

--- app/src/main/java/com/example/musicquizapp/MainActivity.java
+++ app/src/main/java/com/example/musicquizapp/MainActivity.java
@@ -7,8 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.musicquizapp.model.ListQuestion;
+import com.example.musicquizapp.model.Question;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +27,31 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, trang_Home.class);
-                startActivity(intent);
+                FirebaseDatabase database = FirebaseDatabase.getInstance("https://quanlycauhoinhac-default-rtdb.asia-southeast1.firebasedatabase.app");
+                DatabaseReference myRef = database.getReference("question");
+
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        ListQuestion lstQuestion = new ListQuestion();
+                        ArrayList<Question> arrLstQuestion = new ArrayList<>();
+                        for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+                            Question data = childSnapshot.getValue(Question.class);
+                            arrLstQuestion.add(data);
+                        }
+                        Intent intent = new Intent(MainActivity.this, My_question.class);
+                        lstQuestion.setMyObjectList(arrLstQuestion);
+                        intent.putExtra("lstQuestionObj",lstQuestion);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Xử lý lỗi (nếu có)
+                    }
+                });
+
+
             }
         });
 
